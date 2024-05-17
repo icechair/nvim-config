96620ee75a6c67978c1a7f13e10ecee798cd8e4c
Martin Gabel <martin.gabel@becker-antriebe.com>
2024-05-17 08:56:29 +0200

    remove old stuff

--- lua/icechair/remap.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc="leave file"})
-vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set("n", "<leader>b", function()
-        vim.o.list = not vim.o.list
-end, {desc ="toggle whitespaces"})
-
